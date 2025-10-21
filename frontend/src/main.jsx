8eebcf142b6321eb381f2c2f30c68259489779e1
mithlesh <kamaldewangan367@gmail.com>
2025-10-21 19:57:51 +0530

    almost complete sign up and login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </>
+  )
+);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import { FaFacebookF } from "react-icons/fa6";
+import { SiGmail } from "react-icons/si";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+   try {
+     const response = await axios.post("url",{email,password})
+     if(response.success){
+       setMessage(response.message)
+       setLoading(false);
+       navigate("/");
+     }
+      setMessage(response.message)
+       setLoading(false);
+       return;
+   } catch (error) {
+    setMessage("Something wen wrong, Please try again")
+       setLoading(false);
+       console.error(error)
+   }
+  };
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center p-4">
+      <div className="w-full max-w-sm flex flex-col items-center">
+        {/* Login Box */}
+        <div className="bg-white border border-gray-300 p-8 pt-10 pb-6 mb-3 rounded-lg w-full flex flex-col items-center shadow-sm">
+          {/* Instagram Logo - Font-like appearance */}
+          <h1
+            className="text-5xl font-script text-gray-900 mb-8"
+            style={{ fontFamily: "Pinyon Script, cursive", fontWeight: 600 }}
+          >
+            Tasveera
+          </h1>
+
+          {/* Form Inputs */}
+          <form onSubmit={handleSubmit} className="w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full px-3 py-2 mb-2 text-sm border border-gray-300 rounded-md focus:ring-0 focus:border-gray-500 bg-gray-50 placeholder-gray-500"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full px-3 py-2 mb-4 text-sm border border-gray-300 rounded-md focus:ring-0 focus:border-gray-500 bg-gray-50 placeholder-gray-500"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <p className="text-center text-red-500 font-bold">{message}</p>
+            {/* Log In Button */}
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-1.5 rounded-lg text-sm font-semibold  hover:opacity-100 transition-opacity cursor-pointer"
+            >
+              {loading ? "Loading..." : "Log In"}
+            </button>
+          </form>
+
+          {/* OR Separator */}
+          <div className="flex items-center w-full my-4">
+            <div className="grow border-t border-gray-300"></div>
+            <span className="text-gray-500 text-xs font-semibold mx-4">OR</span>
+            <div className="grow border-t border-gray-300"></div>
+          </div>
+
+          {/* Social Logins  */}
+
+          {/* Facebook Login */}
+          <button
+            className="flex items-center justify-center gap-1 w-full text-blue-800 font-semibold text-sm mb-3"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log("Facebook Login clicked");
+            }}
+          >
+            <FaFacebookF />
+            Log in with Facebook
+          </button>
+
+          {/* Gmail/Google Login */}
+          <button
+            className="flex items-center gap-2 justify-center w-full text-red-600 font-semibold text-sm mb-4"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log("Gmail/Google Login clicked");
+            }}
+          >
+            <SiGmail />
+            Log in with Gmail
+          </button>
+        </div>
+
+        {/* Sign Up Link */}
+        <div className="bg-white border border-gray-300 p-5 rounded-lg w-full text-center text-sm shadow-sm">
+          <p className="text-gray-900">
+            Don't have an account?
+            <Link
+              to="/signup"
+              className="text-blue-500 font-semibold ml-1 cursor-pointer"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
