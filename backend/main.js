682bd5fcf612b4b6174dfe0cb3322f95fab46c74
mithlesh <kamaldewangan367@gmail.com>
2025-12-17 16:33:44 +0530

    installed express and set up backend

---
+++ backend/main.js
@@ -0,0 +1,13 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+console.log(process.env.PORT)
+const port = process.env.PORT | 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`)
+})
