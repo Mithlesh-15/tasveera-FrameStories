ce559c86ad3ba977ea66e6767f1002832176202e
mithlesh <kamaldewangan367@gmail.com>
2025-12-18 13:53:11 +0530

    creacted function for connect db

---
+++ backend/lib/connectDB.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed");
+    console.error(error.message);
+
+    // process ko yahin band kar do
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/main.js
+++ backend/main.js
@@ -1,14 +1,16 @@
-import express from express
-import dotenv from dotenv
-dotenv.config()
-const app = express()
-console.log(process.env.PORT)
-const port = process.env.PORT | 4000
+import connectDB from "./lib/connectDb.js";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+connectDB();
+const app = express();
+console.log(process.env.PORT);
+const port = process.env.PORT | 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+  console.log(`http://localhost:${port}/`);
+});
