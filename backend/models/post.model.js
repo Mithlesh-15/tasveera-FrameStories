bcd6e132c16081994b04a8922d1ce830ec27b139
mithlesh <kamaldewangan367@gmail.com>
2025-12-18 13:44:36 +0530

    created post schema

---
+++ backend/models/post.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    isOwner: {
+      type: Boolean,
+      default: true,
+    },
+
+    dataLink: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: ["image", "video"],
+      required: true,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    caption: {
+      type: String,
+      default: "",
+      maxlength: 50,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
