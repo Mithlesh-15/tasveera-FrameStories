c1f0f3604831fd7db1df68a3ad792fad749b76bd
mithlesh <kamaldewangan367@gmail.com>
2025-12-27 15:35:09 +0530

    added route

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import upload from "../middleware/upload.js"
+import { createPost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-
+router.post("/upload",upload.single("media"),createPost)
 
 
 export default router;
