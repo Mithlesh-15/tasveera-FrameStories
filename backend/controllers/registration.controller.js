91ac2fed8eb17674d7567a3be704e791c70ba183
mithlesh <kamaldewangan367@gmail.com>
2026-01-11 13:54:35 +0530

    fix: allow cross-site auth cookies for production

--- backend/controllers/registration.controller.js
+++ backend/controllers/registration.controller.js
@@ -11,8 +11,9 @@ const JWTandCookieSetUp = (res, userId) => {
   // Set token in cookie
   res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
 
