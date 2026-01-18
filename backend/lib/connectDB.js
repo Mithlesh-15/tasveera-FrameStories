4dbba44418f8797eeff563464b895b745701ccfc
mithlesh <kamaldewangan367@gmail.com>
2026-01-18 15:44:29 +0530

    implement Message schema for chat system

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/tasveera");
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
---
+++ backend/models/message.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+      index: true,
+    },
+
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    text: {
+      type: String,
+      trim: true,
+      maxlength: 1000,
+      required: true,
+    },
+
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// fast chat loading
+messageSchema.index({ conversationId: 1, createdAt: 1 });
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
