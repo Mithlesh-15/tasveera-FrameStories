11cec30f34964f6c04a5f837c171579252bc10a4
mithlesh <kamaldewangan367@gmail.com>
2026-01-25 11:33:05 +0530

    add Global socket connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MenuBar from "./components/MenuBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./utils/scrollBarHide.css";
+import api from "./api/axios";
+import toast from "react-hot-toast";
+import { socket } from "./utils/socket";
 
 function App() {
+  const nevigate = useNavigate();
+  const [currentUserId, setCurrentUserId] = useState(null);
+  const fetchCurrentUserId = async () => {
+    try {
+      const response = await api.get("/api/v1/get-current-user-id");
+      setCurrentUserId(response.data.id);
+    } catch (error) {
+      console.log("Fetch User Error : ", error);
+      const status = error?.response?.status;
+      const message = error?.response?.data?.message || "Something went wrong";
+
+      toast.error(message);
+
+      if (status === 401) {
+        toast.error("Please login first");
+        nevigate("/login");
+      }
+    }
+  };
+  useEffect(() => {
+      fetchCurrentUserId();
+      if (!currentUserId) return;
+  
+      socket.connect();
+      socket.emit("join", currentUserId);
+  
+      return () => {
+        socket.disconnect();
+      };
+    }, [currentUserId]);
   return (
     <>
       <div className="min-h-screen bg-gray-50 scrollbar-hide">
