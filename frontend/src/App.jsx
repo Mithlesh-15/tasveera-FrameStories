ae2c7d04fbd24f01d5b57f1a69d1bebc0dc945a6
mithlesh <kamaldewangan367@gmail.com>
2026-01-25 11:43:47 +0530

    fix unnecessary login message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import MenuBar from "./components/MenuBar";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./utils/scrollBarHide.css";
 import api from "./api/axios";
-import toast from "react-hot-toast";
 import { socket } from "./utils/socket";
 
 function App() {
-  const nevigate = useNavigate();
   const [currentUserId, setCurrentUserId] = useState(null);
   const fetchCurrentUserId = async () => {
     try {
@@ -15,28 +13,19 @@ function App() {
       setCurrentUserId(response.data.id);
     } catch (error) {
       console.log("Fetch User Error : ", error);
-      const status = error?.response?.status;
-      const message = error?.response?.data?.message || "Something went wrong";
-
-      toast.error(message);
-
-      if (status === 401) {
-        toast.error("Please login first");
-        nevigate("/login");
-      }
     }
   };
   useEffect(() => {
-      fetchCurrentUserId();
-      if (!currentUserId) return;
-  
-      socket.connect();
-      socket.emit("join", currentUserId);
-  
-      return () => {
-        socket.disconnect();
-      };
-    }, [currentUserId]);
+    fetchCurrentUserId();
+    if (!currentUserId) return;
+
+    socket.connect();
+    socket.emit("join", currentUserId);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [currentUserId]);
   return (
     <>
       <div className="min-h-screen bg-gray-50 scrollbar-hide">
